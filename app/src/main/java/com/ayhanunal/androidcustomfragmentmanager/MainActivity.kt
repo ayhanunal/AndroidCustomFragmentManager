0be2ef1132bffac5485d61b30aaaa933aad58db4
ayhanunal <unalayhan@yahoo.com>
2021-11-19 10:07:32 +0300

    view binding implementation done

--- app/src/main/java/com/ayhanunal/androidcustomfragmentmanager/MainActivity.kt
+++ app/src/main/java/com/ayhanunal/androidcustomfragmentmanager/MainActivity.kt
@@ -2,10 +2,18 @@ package com.ayhanunal.androidcustomfragmentmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ayhanunal.androidcustomfragmentmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
     }
 }
--- app/src/main/java/com/ayhanunal/androidcustomfragmentmanager/fragments/FragmentB.kt
+++ app/src/main/java/com/ayhanunal/androidcustomfragmentmanager/fragments/FragmentB.kt
@@ -1,9 +1,30 @@
 package com.ayhanunal.androidcustomfragmentmanager.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ayhanunal.androidcustomfragmentmanager.databinding.FragmentBBinding
 import com.ayhanunal.androidcustomfragmentmanager.fragments.infrastructure.MyFragment
 import com.ayhanunal.androidcustomfragmentmanager.fragments.infrastructure.MyFragmentType
 
 class FragmentB : MyFragment() {
+
+    private lateinit var binding: FragmentBBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 
     override val fragmentType: MyFragmentType
         get() = MyFragmentType.MY_FRAGMENT_TYPE_B
